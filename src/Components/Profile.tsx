4d6d5e5acff555ec51694605ab7ef9b58ba295bf
cuingskot76 <afrizapamujo@gmail.com>
2023-03-27 18:26:58 +0700

    new finish

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,71 +1,110 @@
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, Image, TextInput, StyleSheet, Button} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 
 const Profile = () => {
   return (
-    <View style={styles.container}>
-      {/* images */}
-      <View style={styles.images}>
-        <View style={styles.image}>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1640682567682-472e5e9eb889?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1953&q=80',
-            }}
-            style={{width: '100%', height: '100%'}}
-          />
-        </View>
-        <View style={styles.image}>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1640682567682-472e5e9eb889?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1953&q=80',
-            }}
-            style={{width: '100%', height: '100%'}}
-          />
-        </View>
-      </View>
-      {/* __end-images */}
-
-      <View style={styles.descContainer}>
-        {/* description */}
-        <Text style={styles.description}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
-          officia, commodi amet fugit recusandae soluta nisi fugiat dignissimos,
-          autem, vero cupiditate possimus sunt ut itaque.
+    <>
+      <View style={styles.headcop}>
+        <Text style={{fontWeight: 'bold', fontSize: 20, color: 'black'}}>
+          Welcome back, Afrzl
         </Text>
-        {/* en__description */}
-
-        {/* input */}
-        <TextInput placeholder="Enter your name" style={styles.input} />
-        {/* __end-input */}
-
-        {/* profile-user */}
-        <View style={styles.userImage}>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1640682567682-472e5e9eb889?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1953&q=80',
-            }}
-            style={{
-              width: 150,
-              height: 150,
-              borderRadius: 100,
-            }}
-          />
-        </View>
-        {/* __end-profile-user */}
-
-        {/* actions */}
-        <View style={styles.actions}>
-          <View style={{width: '40%'}}>
-            <Button title="Keluar" color="#C84361" />
-          </View>
-          <View style={{width: '40%'}}>
-            <Button title="Simpan" color="#57C5B6" />
-          </View>
-        </View>
-        {/* __end-actions */}
       </View>
-    </View>
+      <ScrollView style={styles.container}>
+        {/* images */}
+        <View style={styles.images}>
+          <View style={styles.image}>
+            <Image
+              source={require('../../public/images/mech-1.jpg')}
+              style={{width: '100%', height: '100%', borderRadius: 10}}
+            />
+          </View>
+          <View style={styles.image}>
+            <Image
+              source={require('../../public/images/mech-2.jpg')}
+              style={{width: '100%', height: '100%', borderRadius: 10}}
+            />
+          </View>
+        </View>
+        {/* __end-images */}
+
+        <View style={styles.descContainer}>
+          {/* description */}
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 18,
+              textAlign: 'center',
+              marginBottom: 7,
+            }}>
+            So, who am i?
+          </Text>
+          <Text style={styles.description}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus
+            officia, commodi amet fugit recusandae soluta nisi fugiat
+            dignissimos, autem, vero cupiditate possimus sunt ut itaque.
+          </Text>
+          {/* en__description */}
+
+          {/* input */}
+          <TextInput placeholder="Enter your name" style={styles.input} />
+          {/* __end-input */}
+
+          {/* profile-user */}
+          <View style={styles.userImage}>
+            <Image
+              source={require('../../public/images/user.png')}
+              style={{
+                width: 200,
+                height: 200,
+                borderRadius: 100,
+                borderWidth: 1,
+              }}
+            />
+          </View>
+          {/* __end-profile-user */}
+
+          {/* actions */}
+          <View style={styles.actions}>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#FF2E63',
+                display: 'flex',
+                justifyContent: 'center',
+                paddingVertical: 10,
+                paddingHorizontal: 40,
+                marginRight: 20,
+                alignItems: 'center',
+                borderRadius: 50,
+              }}>
+              <Text style={styles.textAction}>Back</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#3E54AC',
+                display: 'flex',
+                justifyContent: 'center',
+                paddingVertical: 10,
+                paddingHorizontal: 40,
+                marginRight: 20,
+                alignItems: 'center',
+                borderRadius: 50,
+              }}>
+              <Text style={styles.textAction}>Save</Text>
+            </TouchableOpacity>
+          </View>
+          {/* __end-actions */}
+        </View>
+      </ScrollView>
+    </>
   );
 };
 
@@ -73,10 +112,21 @@ const styles = StyleSheet.create({
   container: {
     height: '100%',
     paddingHorizontal: 20,
+    backgroundColor: '#ffffff',
   },
 
   descContainer: {
-    backgroundColor: 'salmon',
+    backgroundColor: '#f2f1f7',
+    padding: 20,
+  },
+
+  headcop: {
+    backgroundColor: '#d1cfdd',
+    paddingVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 3,
+    marginBottom: 20,
   },
 
   images: {
@@ -87,20 +137,23 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: 150,
-    height: 150,
+    width: 170,
+    height: 170,
   },
 
   description: {
     textAlign: 'center',
+    color: 'gray',
+    fontSize: 16,
+    lineHeight: 20,
   },
 
   input: {
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: '#aaa',
     borderRadius: 10,
     padding: 10,
-    marginVertical: 20,
+    marginVertical: 40,
   },
 
   userImage: {
@@ -110,8 +163,13 @@ const styles = StyleSheet.create({
   actions: {
     display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+
+  textAction: {
+    color: 'white',
+    fontSize: 16,
   },
 });
 
